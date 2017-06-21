6b7c265e12f2bc0a931afc14ae21aa5632e58f33
wangfenglai <wfl0314zy@163.com>
2017-06-21 03:51:46 -0400

    quick
    quick

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import{ Component } from '@angular/core';
+
+@Component({
+	selector:'my-app',
+	template:'<h1>我的一个计算机老师</h1>'
+
+})
+export class AppComponent{ }
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import{paltformBrowserDynamic }from '@angular/platform-browser-dynamic';
+import{ AppModule } from './app.module';
+
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule); 
